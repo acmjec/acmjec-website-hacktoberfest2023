6c6d87f827761169f08f4c73b9dcbe46179dbfba
A-R-P-I-T-JAIN <116783129+A-R-P-I-T-JAIN@users.noreply.github.com>
2023-10-13 09:42:14 +0530

    updated main landing page

--- src/components/MainLanding/MainLanding.jsx
+++ src/components/MainLanding/MainLanding.jsx
@@ -3,7 +3,7 @@ import './MainLanding.css'
 import logo from '../../../public/images/ACM JEC Logo.png'
 import SessionCard from './SessionCard'
 import grid from '../../../public/images/grid.png'
-import JoinUsCards from '../why_join_us/JoinUsCards'
+import lines from '../../../public/images/lines.png'
 
 const MainLanding = () => {
   return (
@@ -11,8 +11,14 @@ const MainLanding = () => {
       <img src={grid} alt="" className="grid" />
       <img src={logo} alt="" className='logo'/>
       <div className="content">
-        <h3>UNLOCK YOUR POTENTIAL WITH</h3>
-        <h1><span>ACM JEC</span> STUDENT'S CHAPTER</h1>
+        <div className="temp">
+          <img src={lines} alt="" className="left" />
+          <div className="temp1">
+          <h3>UNLOCK YOUR POTENTIAL WITH</h3>
+          <h1><span>ACM JEC</span> STUDENT'S CHAPTER</h1>
+          </div>
+          <img src={lines} alt="" className="right" />
+        </div>
         <h2>YOUR GATEWAY TO TECH EXCELLENCE</h2>
       </div>
 
