f639eb7e09393e015d73acb3e54e4ad4e17e7e1f
RAJ MANDAL <usingrajcseinfo@gmail.com>
2023-10-15 14:07:50 +0530

    Join us card hover effect

--- src/components/why_join_us/JoinUsCards.jsx
+++ src/components/why_join_us/JoinUsCards.jsx
@@ -4,13 +4,13 @@ function JoinUsCards(props) {
 
   return (
     <>
-      <div className="card xl:px-6 px-2 text-white text-center h-80 xl:w-64 w-56 bg-[#222125] mb-24" style={{borderRadius:'60px',filter:"opacity(1)"}}>
+      <div className={`group/item card xl:px-6 px-2 text-white text-center md:h-96 xl:w-64 w-56 bg-[#222125] mb-24 cursor-pointer hover:shadow-[0_2px_15px_white] shadow-none hover:h-[25rem]`} style={{borderRadius:'2rem',filter:"opacity(1)", }}>
         <div className='relative bottom-24 flex flex-col items-center justify-between'>
-        <img src={props.src} alt="image" className='w-44 h-48 xl:mb-5 lg:mb-3'/>
+        <img src={props.src} alt="image" className={`w-44 h-48 xl:mb-5 lg:mb-3 relative group-hover/item:bottom-8`}/>
         <div className="title font-bold text-2xl mb-2">{props.title}</div>
-        <p className="description text-xs font-[Montserrat] flex-1">{props.description}</p>
+        <p className="description text-xs font-[Montserrat] flex-1 group-hover/item:text-sm">{props.description}</p>
 
-        <button className="action rounded-full bg-[#2E2D32] px-12 py-2 text-[#F1F379] mt-5">{props.action}</button>
+        <button className="action relative top-14 rounded-full bg-[#2E2D32] px-12 py-2 text-[#F1F379] pb-2">{props.action}</button>
         </div>
       </div>
     </>
