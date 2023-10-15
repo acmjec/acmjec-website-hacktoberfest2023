83b96ac3887764adc2d8986919285317e10d79ac
Akshat Singh Thakur <akshatthakur7424@gmail.com>
2023-10-15 19:39:11 +0530

    adding spinner on page load

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import LandingPage from "./components/LandingPage";
+import Spinner from "./components/Spinner/Spinner";
+import JoinUs from "./components/why_join_us/Join_us";
 
 function App() {
+
+  const [isLoading, setloading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setloading(false);
+    }, 3000);
+  }, [])
+
+
   return (
     <div className="bg-black">
       {/* <Navbar /> */}
-      <LandingPage />
+      {isLoading ? <Spinner /> : <LandingPage />}
     </div>
   );
 }
---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./spinner.css"
+
+const Spinner = () => {
+    return (
+        <>
+        <div className="spinnerClass">
+            <div className="loadingio-spinner-ripple-8ugwl6ev0hj"><div className="ldio-f0w8taiz7k4 bg-black">
+                <div></div><div></div>
+            </div></div>
+        </div>
+        </>
+    )
+}
+
+export default Spinner
