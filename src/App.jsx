f87a765a7536f9112d5ab09ba8ce885a3074771d
isha.ghyar <isha.ghyar@quantiphi.com>
2023-10-15 19:20:32 +0530

    signup ui complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import LandingPage from "./components/LandingPage";
+import Signup from "./components/SignupPage/Signup";
 
 function App() {
   return (
     <div className="bg-black">
       {/* <Navbar /> */}
       <LandingPage />
+      {/* <Signup /> */}
     </div>
   );
 }
