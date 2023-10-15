bbe64af973214e08f8fb2d83d3306f4caeaf3e51
isha.ghyar <isha.ghyar@quantiphi.com>
2023-10-15 22:45:54 +0530

    signup route made

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import LandingPage from "./components/LandingPage";
 import Signup from "./components/SignupPage/Signup";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="bg-black">
       {/* <Navbar /> */}
-      <LandingPage />
       {/* <Signup /> */}
+      <Router>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />}/>
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
