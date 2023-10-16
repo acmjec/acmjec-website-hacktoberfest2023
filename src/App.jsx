083e3c644c00442bf51a4b5c5422ffb6a1cc41ae
Akshat Singh Thakur <akshatthakur7424@gmail.com>
2023-10-16 15:25:30 +0530

    Fixing css error in join us cards section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import LandingPage from "./components/LandingPage";
+import Spinner from "./components/Spinner/Spinner";
+import JoinUs from "./components/why_join_us/Join_us";
+import Signup from "./components/SignupPage/Signup";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 
@@ -16,7 +20,13 @@ function App() {
   return (
     <div className="bg-black">
       {/* <Navbar /> */}
-      <LandingPage />
+      {/* {isLoading ? <Spinner /> : <LandingPage />} */}
+      <Router>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />}/>
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
