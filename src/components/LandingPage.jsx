14c0ea75e906ec82ab5f3e7bceaf1d69cc0606a2
Bhavesh <diwalebhavesh@gmail.com>
2023-10-18 14:06:05 +0530

    Add Footer and Footer Card

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import MainLanding from './MainLanding/MainLanding'
 import Join_us from './why_join_us/Join_us'
+import MeetTeam from './MeetOurTeam/MeetTeam'
+import Footer from './Footer/Footer'
 
 function LandingPage() {
   return (
     <div className='text-blue-300 text-2xl overflow-x-hidden overflow-y-hidden'>
       <MainLanding />
       <Join_us />
+      <Footer/>
     </div>
   )
 }
