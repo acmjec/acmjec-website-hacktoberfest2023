4413112e210d134ea595656bff2da40871330008
atharv-av <atharvv333@gmail.com>
2023-10-21 15:07:05 +0530

    Fixed bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,10 @@
-import React, { Fragment, useEffect, useRef } from "react";
+import React, { Fragment, useRef } from "react";
 import styles from "./Navbar.module.css";
 import logo from "../../public/images/mainLogo.png";
 
-// window.onload = function(){
-  
-
-// }
-
 function Navbar() {
-
   const mobile_view = useRef();
-  
+
   function menu(e) {
     e.target.classList.toggle(styles["is_active"]);
     mobile_view.current.classList.toggle(styles["is_active"]);
@@ -24,11 +18,7 @@ function Navbar() {
             <img src={logo} alt="" />
           </div>
 
-
-          <div className={styles.content} >
-
           <div className={styles.content}>
-
             <a href="#">Home</a>
             <a href="#">About Us</a>
             <a href="#">Members</a>
@@ -39,7 +29,7 @@ function Navbar() {
             Join Us
           </a>
           <div className={styles.search_space}>
-            <i class="fa-solid fa-magnifying-glass"></i>
+            <i className="fa-solid fa-magnifying-glass"></i>
             <input
               type="search"
               placeholder="Search...."
@@ -52,27 +42,25 @@ function Navbar() {
         </div>
       </nav>
       <nav className={styles.mobile_view} ref={mobile_view}>
-          <a href="#">Home</a>
-          <a href="#">About Us</a>
-          <a href="#">Members</a>
-          <a href="#">Editorial Board</a>
-          <a href="#">Resources</a>
-            <a href="#" className={styles.joinus_a}>
-              Join Us
-            </a>
-          <div className={styles.search_space_a}>
-            <i class="fa-solid fa-magnifying-glass"></i>
-            <input
-              type="search"
-              placeholder="Search...."
-              className={styles.search}
-            />
-          </div>
+        <a href="#">Home</a>
+        <a href="#">About Us</a>
+        <a href="#">Members</a>
+        <a href="#">Editorial Board</a>
+        <a href="#">Resources</a>
+        <a href="#" className={styles.joinus_a}>
+          Join Us
+        </a>
+        <div className={styles.search_space_a}>
+          <i className="fa-solid fa-magnifying-glass"></i>
+          <input
+            type="search"
+            placeholder="Search...."
+            className={styles.search}
+          />
+        </div>
       </nav>
     </Fragment>
   );
 }
 
-
 export default Navbar;
-
